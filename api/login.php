7b5ac4a4fa08c23ec390790e9acd08771163eca8
KenTaghap <109163116+KenTaghap@users.noreply.github.com>
2023-11-28 22:21:54 +0800

    Update login.php

--- api/login.php
+++ api/login.php
@@ -26,75 +26,93 @@ $user = $collection->findOne(['username' => $username, 'password' => $password])
 if ($user) {
     // User is authenticated
     echo '
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-              <meta charset="UTF-8">
-              <title>Blue Background Page</title>
-              <style>
-                body {
-                  background-color: #0077b6; /* Set the background color to blue */
-                  color: white; /* Set text color to white for better visibility */
-                  font-family: Arial, sans-serif; /* Set font for better readability */
-                  margin: 0; /* Remove default margin */
-                  padding: 20px; /* Add some padding for content */
-                }
-              </style>
-            </head>
-            <body>
-            <center>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br><br>
-            <h1> Login successful! </h1>
-            <button><a href="../api/index.php">Proceed</a></button>
-            </center>
-            </body>
-            </html>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>Blue Background Page</title>
+      <style>
+        body {
+          background-color: #ADD8E6; /* Light blue background */
+          color: white;
+          font-family: "Times New Roman", Times, serif; /* Times New Roman font */
+          margin: 0;
+          padding: 20px;
+          text-align: center; /* Center align the content */
+        }
+        /* Style the button separately */
+        button {
+          padding: 20px 35px; /* Larger padding to increase button size */
+          background-color: #ffffff;
+          color: #0077b6;
+          border: none;
+          cursor: pointer;
+          text-decoration: none; /* Remove underline on hover */
+          font-size: 18px; /* Increase font size */
+          margin-top: 20px; /* Add margin to the button */
+        }
+        /* Style the button hover effect */
+        button:hover {
+          background-color: #f0f0f0;
+        }
+      </style>
+    </head>
+    <body>
+      <br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <h1 style="color:black;"> Login successful! </h1>
+      <!-- Use a div or a different container instead of a button inside an anchor tag -->
+      <div>
+        <a href="../api/index.php" >
+          <button>Proceed</button>
+        </a>
+      </div>
+    </body>
+    </html>
             ';
 } else {
     // Invalid login
     echo '
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-              <meta charset="UTF-8">
-              <title>Blue Background Page</title>
-              <style>
-                body {
-                  background-color: #0077b6; /* Set the background color to blue */
-                  color: white; /* Set text color to white for better visibility */
-                  font-family: Arial, sans-serif; /* Set font for better readability */
-                  margin: 0; /* Remove default margin */
-                  padding: 20px; /* Add some padding for content */
-                }
-              </style>
-            </head>
-            <body>
-            <center>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br><br>
-            <h1> Invalid username or password. </h1>
-            <button><a href="../index.html">Go Back</a></button>
-            </center>
-            </body>
-            </html>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>Blue Background Page</title>
+      <style>
+        body {
+          background-color: #ADD8E6; /* Light blue background */
+          color: white;
+          font-family: "Times New Roman", Times, serif; /* Times New Roman font */
+          margin: 0;
+          padding: 20px;
+          text-align: center; /* Center align the content */
+        }
+        /* Style the button separately */
+        button {
+          padding: 20px 35px; /* Larger padding to increase button size */
+          background-color: #ffffff;
+          color: #0077b6;
+          border: none;
+          cursor: pointer;
+          text-decoration: none; /* Remove underline on hover */
+          font-size: 18px; /* Increase font size */
+          margin-top: 20px; /* Add margin to the button */
+        }
+        /* Style the button hover effect */
+        button:hover {
+          background-color: #f0f0f0;
+        }
+      </style>
+    </head>
+    <body>
+      <br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <h1 style="color:black;"> Invalid username or password. </h1>
+      <!-- Use a div or a different container instead of a button inside an anchor tag -->
+      <div>
+        <a href="../index.html" >
+          <button>Go Back</button>
+        </a>
+      </div>
+    </body>
+    </html>
             ';
 }
